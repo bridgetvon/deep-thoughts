170ddfa43115e43de1379ed0eaa10a377da1847c
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-03 12:29:35 -0700

    graphQl queries tested

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,40 @@
+const { User, Thought } = require('../models');
+
+
 //create resolver to serve the response to the helloworld query 
 const resolvers = {
     Query: { 
-        helloWorld: () => {
-            return 'Hello world!'
+       thoughts: async (parent, {username}) => {
+           //parent is a placeholder parameter so we can acces the username for the second parameter 
+           const params = username ? { username } : {};
+           return Thought.find().sort({ createdAt: -1 });
+        },
+        //get thought by id
+        thought: async (parent, {_id}) => {
+            return Thought.findOne({ _id });
+        },
+        //get all users 
+        users: async () => {
+            return User.find()
+            .select('-__v  -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
+        //get user by username 
+        user: async (parent, {username}) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
         }
     }
 };
+
+//using parameters so apoolo server library can pass data so we can have a more dynamic interaction with our servver
+//the resolver can accept four arguments in the following order 
+//1. parent: this is if we used nested resolvers to handle more complicated actions as it would hold reference to the resolver that executed the nested resolver function 
+//2. args: this is an object of all the values passed into a query or mutation request as parameters 
+//3. context: if we were to need the same data to be accessible by all resolvers such as logged in user status or API tokens this data will come through the context parameter 
+//4. info: this will contain extra information about an operations current state 
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,10 +7,44 @@ const { gql } = require('apollo-server-express');
 //create typedefs 
 //type query datType {} is how you define a query 
 const typeDefs = gql`
+    type Thought {
+        _id: ID
+        thoughtText: String
+        createdAT: String
+        username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
+
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        friendCount: Int 
+        thoughts: [Thought]
+        friends: [User]
+    }
+
     type Query {
-        helloWorld: String
+        users: [User]
+        user(username: String!): User
+        thoughts(username: String): [Thought]
+        thought(_id: ID!): Thought
     }
 `;
+//(usernameL String) defined thoughts so that it could recieve a parameter if we wanted 
+//the parameter would be username and would have a string data type 
+
+// the ! excalmmation point indicates that for a query to be carried out the data must exist 
+
+//when we run a thought query we can also list the reactions field to get back an array of reaction data for each though 
 
 //export typeDefs
 module.exports = typeDefs;
